13946d0479b56bebdcae3591f46676c1cf86fec3
Michael Sobczak <michaelsobczak54@gmail.com>
2012-11-26 04:09:30 -0500

    bugfixes for checkers simulator
    got a terminating rough implementation of a checkers simulator, still
    need to verify that it works

--- metasim/metasim/games/Checkers.py
+++ metasim/metasim/games/Checkers.py
@@ -13,78 +13,22 @@ EMPTY = 'Empty'
 jump_tuple_fields = (source, dest, victim, stage) = ('jump source', 'jump dest', 'jump victim', 'jump stage')
 
 isodd = lambda n: n % 2 == 1
-color = lambda x,y: BLACK if isodd(x) == isodd(y) else RED
+tile_color = lambda x,y: BLACK if isodd(x) == isodd(y) else RED
 valid_loc = lambda r,t: r in legal_coords and t in legal_coords
 dist = lambda a,b: abs(a[0]-b[0]) + abs(a[1]-b[1])
 def midpoint(start_point, end_point):
 	sx, sy, ex, ey = start_point + end_point
-	return [ v/2 for v in (sx+ex, sy+ey)]
+	return ((sx+ex)/2, (sy+ey)/2)
 
 def get_neighbors(x, y, distance=1,remove_invalid=True):
 	all_coords = [ (x+(dx*distance),y+(dy*distance)) for dx,dy in normal_movements]
 	return filter(lambda r,s: valid_loc(r,s), all_coords) if remove_invalid else all_coords
 
-def can_jump(jumper_color, jumper_start, over, destination, omap):
-	if not valid_loc(*destination) or omap[destination[0]][destination[1]] != EMPTY:
-		return False
-	return jumper_color != omap[over[0]][over[1]].color and dist(jumper_start,over) == 2 and dist(over,destination) == 2 and dist(jumper_start,destination) == 4
-
-def build_jump(piece, hops):
-	s_hops = sorted(hops, key=lambda h: h[3])
-	mpath = set(reduce(lambda l,r: l + r, [ (hp[0], hp[1]) for hp in s_hops ]))
-	p_rem = [ hp[2] for hp in s_hops ]
-	return Move(piece, JUMP, mpath, p_rem, mpath[-1] == KING_LOC[piece.color])
-
-def valid_nonjump_move(piece, start_location, end_location):
-
-
-
-def _try_jump(piece, target, occupancy_map, stage):
-	src = piece.x, piece.y
-	dst = target
-	btwn = midpoint(src, dst)
-	stg = stage
-	hops = list()
-	while can_jump(piece.color, src, btwn, dst, occupancy_map):
-		hops.append((src, dst, occupancy_map[btwn[0]][btwn[1]], stg))
-		stg += 1
-		src = dst
-
-
-
-def try_jump(piece, target, jumper, occupancy_map):
-	all_hops = _try_jump(piece, target, occupancy_map, 0)
-	if not all_hops:
-		return None
-	# determine max stage and break down hops by stage
-	max_stage = max([hp[stage] for hp in all_hops])
-	stg_iter = lambda: xrange(max_stage+1)
-	hops_from_stg = lambda s: filter(lambda q: q[stage] == s, all_hops)
-
-	full_jumps = list()
-	partial_jumps = None
-
-	for stg in stg_iter():
-
-		for hops in hops_from_stg(stg):
-			if stg == 0:
-				full_jumps.extend(hops)
-				partial_jumps = hops
-				continue
-		if not partial_jumps:
-			break
-		for pj in partial_jumps:
-			if hops[source] == pj[-1][dest]:
-				new_jump = pj + (hops,)
-				full_jumps.append(new_jump)
-				partial_jumps.append(new_jump)
-		longest = max([ len(pj) for pj in partial_jumps])
-		partial_jumps = filter(lambda opj: len(opj) == longest, partial_jumps)
-
-	for pj in partial_jumps:
-		if not pj in full_jumps:
-			full_jumps.append(pj)
-	return full_jumps
+def legally_directed_neighbors(c, x, y, d=1, all_le_d=False):
+	if not all_le_d:
+		return filter(lambda n_y: y > n_y if c is BLACK else y < n_y, get_neighbors(x,y,distance=d))
+	else:
+		return zip(*[ get_neighbors(x,y,distance=current_distance,remove_invalid=False) for current_distance in xrange(1, d+1) ])
 
 
 normal_movements = [ (-1, -1), (-1, 1), (1, 1), (1, -1) ]
@@ -122,9 +66,8 @@ def layout_pieces():
 		pieces.extend([ rc_map[row], (2*n)+s, row ] for n in xrange(4))
 	return [ Piece(c, x, y, False) for c, x, y in pieces ]
 
-
 Piece = namedtuple('Piece', ['color', 'x', 'y', 'is_king'])
-Move = namedtuple('Move', ['piece', 'action', 'movement_path', 'pieces_removed', 'was_made_king'])
+Move = namedtuple('Move', ['piece', 'action', 'movement_path', 'was_made_king',])
 TURN_LIMIT = 5
 
 class Checkers(object):
@@ -136,9 +79,10 @@ class Checkers(object):
 		self.turn = 0
 		self.game_over = False
 		self.piece_count = dict(zip(COLORS, [12] * 2))
+		self.color_map = dict(zip([BLACK, RED], self.players))
 
 	def run(self):
-		self.play_game()
+		return self.play_game()
 
 	def color_this_turn(self):
 		return RED if isodd(self.turn_idx) else BLACK
@@ -148,52 +92,94 @@ class Checkers(object):
 		initial_state = layout_pieces()
 
 		game_state = initial_state
-		active_player = None
-		self.first_player = self.players[0]
+		last_size = len(game_state)
 		while not self.game_over:
-			active_player = self.first_player if active_player != self.first_player else filter(lambda b: b != self.first_player, self.players).pop()
-			game_state = self.play_turn(active_player, game_state)
-
+			game_state = self.play_turn(BLACK if self.turn_idx % 2 == 0 else RED, game_state)
+			new_size = len(game_state)
+			if new_size < last_size:
+				print '%d pieces left' % new_size
+				last_size = new_size
+			#if self.turn_idx > 0 and self.turn_idx % 10 == 0:
+				#print 'Done with %d turns' % self.turn_idx
 			self.turn_idx += 1
-		self.final_state = game_state
 
-	@rules_enforced
-	def find_moves(self, color, pieces, occ_map):
-		all_moves = list()
-		for p in pieces:
-			for nm_x, nm_y in get_neighbors(p.x, p.y):
-				if occ_map[nm_x][nm_y] == EMPTY:
-					all_moves.append(Move(p, MOVE, ((nm_x, nm_y),), None, False))
-				elif occ_map[nm_x][nm_y].color != p.color:
-					jump_moves = try_jump(p, (nm_x, nm_y), (p.x, p.y), occ_map)
-					if jump_moves:
-						all_moves.extend(jump_moves)
+		self.final_state = game_state
+		return game_state
+
+	def find_piece_moves(self, piece, occupancy_map):
+		moves = list()
+		#print str(legally_directed_neighbors(piece.color, piece.x, piece.y, d=2, all_le_d=True))
+		for d1n, d2n in legally_directed_neighbors(piece.color, piece.x, piece.y, d=2, all_le_d=True):
+
+			d1x, d1y = d1n if d1n else (None, None)
+			d2x, d2y = d2n if d2n else (None, None)
+			nbr = occupancy_map[d1x][d1y]
+			if valid_loc(*d1n) and occupancy_map[d1n[0]][d1n[1]] == EMPTY:
+				moves.append(Move(piece, MOVE, [(d1x,d1y)], d1y == 0 and piece.color == BLACK or d1y == BOARD_SIZE-1 and piece.color == RED))
+			elif valid_loc(*d2n) and nbr.color != piece.color and occupancy_map[d2x][d2y] == EMPTY:
+				occupancy_map_copy = occupancy_map.copy()
+				fp = Piece(piece.color, d2x, d2y, piece.is_king or (d2y == 0 and piece.color == BLACK or d2y == BOARD_SIZE-1 and piece.color == RED))
+				occupancy_map_copy[d2x][d2y] = fp
+				occupancy_map_copy[d1x][d1y] = EMPTY
+				occupancy_map_copy[piece.x][piece.y] = EMPTY
+				nbr_jumps = filter(lambda x: x.action == JUMP, self.find_piece_moves(fp, occupancy_map_copy))
+				if nbr_jumps:
+					moves.extend([ Move(piece, JUMP, [d2n] + nj.movement_path, fp.is_king or nj.was_made_king) for nj in nbr_jumps])
+				else:
+					moves.append(Move(piece, JUMP, [d2n], fp.is_king))
+		return moves
+
+	def find_moves(self, move_color, pieces, occ_map):
+		all_moves = reduce(lambda x,y: x + y, map(lambda p: self.find_piece_moves(p, occ_map), filter(lambda ap: ap.color == move_color, pieces)))
 		if not all_moves:
 			self.game_over = True
-		return all_moves
+		has_jump = False
+		for m in all_moves:
+			if m.action == JUMP:
+				has_jump = True
+				break
+		return all_moves if not has_jump else filter(lambda m: m.action == JUMP, all_moves)
+
+
 
 	def apply_move(self, move, pieces):
-		dead_pieces = []
-		if move.pieces_removed:
-			dead_pieces = filter(lambda ded_pc: ded_pc, move.pieces_removed)
-			if dead_pieces:
-				for dp in dead_pieces:
-					self.piece_count[dp.color] =- 1
-		m = move
-		dx, dy = m.movement_path[-1] if isinstance(m.movement_path, Iterable) else m.movement_path
-		replacement_piece = Piece(m.piece.color, dx, dy, m.was_made_king)
-		return filter(lambda p: not p in dead_pieces, map(lambda q: replacement_piece if m.piece == q else q, pieces ))
+		removed_locations = list()
+		if move.action == JUMP:
+			src = (move.piece.x, move.piece.y)
+			for mp in move.movement_path:
+				removed_locations.append(midpoint(src, mp))
+				src = mp
+		dstx, dsty = move.movement_path[-1]
+		new_pieces = filter(lambda np: not (np.x, np.y) in removed_locations, map(lambda ap: Piece(ap.color, dstx, dsty, move.was_made_king) if ap == move.piece else ap, pieces))
+		found_colors = list()
+		for np in new_pieces:
+			if not np.color in found_colors:
+				found_colors.append(np.color)
+				if len(found_colors) == 2:
+					break
+		if len(found_colors) < 2:
+			self.game_over = True
+		return new_pieces
 
-	def play_turn(self, active_player, game_state):
-		print 'Beginning turn %d, %s turn' % (self.turn_idx, active_player.name)
-		p_map = defaultdict(lambda: defaultdict(lambda: EMPTY))
+	def play_turn(self, player_color, game_state):
+		new_occupancy_map = defaultdict(lambda: defaultdict(lambda: EMPTY))
 		for p in game_state:
-			p_map[p.x][p.y] = p
+			new_occupancy_map[p.x][p.y] = p
 
-		#print 'active player is %s and is found in the color dict? %s' % (active_player.name, active_player in self.player_color)
-		possible_moves = self.find_moves(self.color_this_turn, game_state, p_map)
-		chosen_move = active_player.select(possible_moves)
-		return self.apply_move(chosen_move, game_state)
+		possible_moves = self.find_moves(player_color, game_state, new_occupancy_map)
+		if not possible_moves:
+			print 'didn\'t find any moves for %s' % player_color
+			return game_state
+		#else:
+			#print 'found some moves: %s' % str(possible_moves)
+		chosen_move = self.color_map[player_color].select(possible_moves)
+		if chosen_move:
+			#if chosen_move.action == JUMP:
+				#print 'CHOSE A JUMP WOO HOO %s' % str(chosen_move)
+			return self.apply_move(chosen_move, game_state)
+		else:
+			self.game_over = True
+			return game_state
 
 
 class CheckersBot(object):
@@ -208,7 +194,11 @@ def main():
 	#play_game('mike','katie')
 	bots = [ CheckersBot(n) for n in ('mike', 'karn')]
 	fun_game = Checkers(bots)
-	fun_game.run()
+	final_state = fun_game.run()
+	#print 'final state is: %s' % str(final_state)
+	for p in final_state:
+		print '(%d,%d)\t%s\tking?: %s' % (p.x, p.y, p.color, p.is_king)
+
 
 
 
